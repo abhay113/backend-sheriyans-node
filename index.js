7f602388fbb12a4adca7dcbcbd0fcb8889a081ad
abhay <abhay@cateina.com>
2024-06-26 16:49:29 +0530

    passing middlewares

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// app.use((req, res, next) => {
-//   console.log("middleware chaalla");
-//   next(); // kam hone ke baad next middleware execute hoga
-// });
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(accessLog);
-app.use(errorLog);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res
@@ -17,24 +14,13 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
+  app.locals.title = "My App";
   res.send("Hello World!");
 });
 
 app.get("/err", (req, res, next) => {
   return next(new Error("Page not found"));
 });
-function accessLog(req, res, next) {
-  const { hostname, method, path, ip, protocol } = req;
-  console.log(`ACCESS: ${method} ${protocol}://${hostname}${path} - ${ip}`);
-  next();
-}
-
-function errorLog(err, req, res, next) {
-  const { hostname, method, path, protocol } = req;
-  console.log(`ERROR: ${method} ${protocol}://${hostname}${path} - ${err}`);
-  // next(); // you can call either next or send a uniform error response
-  res.status(500).send({ status: "server-error", message: err.message });
-}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
