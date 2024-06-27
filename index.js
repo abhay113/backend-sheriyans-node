351e12cb54375087b63aa96655663a8720782d4b
abhay <abhay@cateina.com>
2024-06-27 10:02:32 +0530

    this setup is comleted

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
