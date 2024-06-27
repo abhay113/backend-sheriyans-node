2fd351b12483e9faf08a8354175632693d43ec4f
abhay <abhay@cateina.com>
2024-06-27 12:54:44 +0530

    jwt token setting to cookie

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 const express = require("express");
 const app = express();
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash("password123", salt, (err, hash) => {
-      console.log(hash);
-      console.log(salt);
-      // Store hash in your password DB.
-    });
-  });
-  res.send("Hello, World!");
+  let token = jwt.sign({ email: "abhay@cateina.com" }, "secretKey");
+  res.cookie("token", token, { expiresIn: "1h" });
+  res.send("Welcome to the API!");
 });
 
-app.get("/check", (req, res) => {
-  bcrypt.compare(
-    "password123",
-    "$2a$10$aQ4zULTkcc9dKnuDzJ6IhODpwop5kdsV5KmGULAKiFmAmr/z4cNua",
-    (err, result) => {
-      if (err) throw err;
-      console.log(result);
-    }
-  );
-  res.send("Hello, World!");
+app.get("/read", (req, res) => {
+  let c = req.cookies;
+  let data = jwt.verify(c.token, "secretKey");
+  console.log("User data: ", data);
+  res.send("User data successfully read!");
 });
-
 app.listen(3000, () => {
   console.log(`Server is running on port 3000!`);
 });
