f64825fcc1c34ee6494121b86b450e8ce5165c28
abhay <abhaychavan346@gmail.com>
2024-06-28 18:17:31 +0530

    completed login logout jwt and isloggedin middleware

--- data-association/routes/route.js
+++ data-association/routes/route.js
@@ -10,6 +10,32 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.send(req.user);
+});
+
+router.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await User.findOne({ email: email });
+  if (!user) return res.status(404).json({ message: "User not found!" });
+
+  bcrypt.compare(password, user.password, (err, isMatch) => {
+    if (!isMatch) return res.status(400).json({ message: "Wrong password!" });
+    let token = jwt.sign({ email: email, userid: user._id }, "secret");
+    res.cookie("token", token);
+    return res.status(200).json({ message: "user logged in successfully" });
+  });
+});
+
+router.get("/logout", async (req, res) => {
+  res.clearCookie("token");
+  return res.redirect("/login");
+});
+
 router.post("/register", async (req, res) => {
   let { email, username, age, password } = req.body;
   let user = await User.findOne({ email: email });
@@ -32,4 +58,13 @@ router.post("/register", async (req, res) => {
   }
 });
 
+function isLoggedIn(req, res, next) {
+  if (!req.cookies.token) {
+    res.send("you must be logged in !!");
+  } else {
+    let data = jwt.verify(req.cookies.token, "secret");
+    req.user = data;
+  }
+  next();
+}
 module.exports = router;
